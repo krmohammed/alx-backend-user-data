4b17f5a560dea8abf607c37cd3b8949b85fe7479
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-09-13 07:57:41 +0000

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ def hash_password(password: str) -> bytes:
     """hashes a password"""
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
