8a6daa50ca6ce6718bd9e95dbe393ca5e75bb090
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-09-13 23:12:14 +0000

    task 2

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Authentication script"""
+from flask import request
+from typing import List
+
+
+class Auth:
+    """Authentication class"""
+
+    def require_auth(self, path: str, exluded_paths: List[str]) -> bool:
+        """Check if the request requires authentication
+        based on the path and excluded paths
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """Retrieve the authorization header from the request
+        """
+        return None
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieve the current user from the request
+        """
+        return None
