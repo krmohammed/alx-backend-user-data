276fba5cca3490cd88d1768715ae12b22e2537c7
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-09-13 06:45:29 +0000

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,16 +6,28 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ["username", "email", "phone", "ssn", "password"]
+
+
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
     """returns a log message obfuscated"""
     obfuscated_message = message
     for f in fields:
-        message = re.sub(
-            f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message
-        )
+        message = re.sub(f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """gets logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    s_handler = logging.StreamHandler()
+    s_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(s_handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
