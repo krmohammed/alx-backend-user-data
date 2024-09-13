7a6896a04253d051a449300a843ae115f53a9900
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-09-13 06:36:45 +0000

    hot fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,9 +11,10 @@ def filter_datum(
 ) -> str:
     """returns a log message obfuscated"""
     obfuscated_message = message
-    for field in fields:
-        message = re.sub(f"{field}=.*?{separator}",
-                            f"{field}={redaction}{separator}", message)
+    for f in fields:
+        message = re.sub(
+            f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message
+        )
     return message
 
 
@@ -29,6 +30,8 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ filter values in incoming log records using filter_datum """
-        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        """filter values in incoming log records using filter_datum"""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
+        )
         return super(RedactingFormatter, self).format(record)
