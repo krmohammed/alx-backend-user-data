9a618d48ad4c8a1f28eb83fb47c43ed73cf1b685
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-09-13 06:11:17 +0000

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" provides functions that handle personal data """
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    """returns a log message obfuscated"""
+    obfuscated_message = message
+    for field in fields:
+        message = re.sub(
+            f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message
+        )
+    return message
