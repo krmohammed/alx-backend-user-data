41640fee364f45fc95e21f375097308a6b894f9a
krmohammed <mohrahman558@gmail.com>
2024-11-20 14:37:06 +0000

    hot fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """filter datum function"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List, redaction: str, message: str, separator: str) -> None:
     """filter datum"""
     for field in message.split(separator):
         if field.split('=')[0] in fields:
